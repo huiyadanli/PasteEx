62437842283643267e46de370f55d45cc25b8850
huiyadanli <huiyadanli@gmail.com>
2017-09-23 01:23:16 +0800

    [-] Delete and sort using list

--- PasteEx/Data.cs
+++ PasteEx/Data.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static PasteEx.Logger;
 
 namespace PasteEx
 {
