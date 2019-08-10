ddfd9b5258f5c9ba6d38b53a87f31f23f110751f
huiyadanli <huiyadanli@gmail.com>
2019-08-11 01:29:42 +0800

    [#] Copyright ~2019

--- PasteEx/Properties/AssemblyInfo.cs
+++ PasteEx/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PasteEx")]
-[assembly: AssemblyCopyright("Copyright © 2017 - 2018")]
+[assembly: AssemblyCopyright("Copyright © 2017 - 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
