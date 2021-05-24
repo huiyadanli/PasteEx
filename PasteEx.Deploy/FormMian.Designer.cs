5b6039112bf5bf92ec71696b272fd9141e09bad2
huiyadanli <huiyadanli@gmail.com>
2021-05-25 00:33:55 +0800

    [+] Deploy helper

---
+++ PasteEx.Deploy/FormMian.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace PasteEx.Deploy
+{
+    partial class FormMian
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVersion = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnUpdateVersion = new System.Windows.Forms.Button();
+            this.txtNewVersion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPackage = new System.Windows.Forms.Button();
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Version:";
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Location = new System.Drawing.Point(163, 59);
+            this.txtVersion.Name = "txtVersion";
+            this.txtVersion.ReadOnly = true;
+            this.txtVersion.Size = new System.Drawing.Size(149, 28);
+            this.txtVersion.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Path:";
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(160, 25);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(35, 18);
+            this.lblPath.TabIndex = 3;
+            this.lblPath.Text = "...";
+            // 
+            // btnUpdateVersion
+            // 
+            this.btnUpdateVersion.Location = new System.Drawing.Point(35, 160);
+            this.btnUpdateVersion.Name = "btnUpdateVersion";
+            this.btnUpdateVersion.Size = new System.Drawing.Size(316, 35);
+            this.btnUpdateVersion.TabIndex = 4;
+            this.btnUpdateVersion.Text = "1. Update";
+            this.btnUpdateVersion.UseVisualStyleBackColor = true;
+            this.btnUpdateVersion.Click += new System.EventHandler(this.btnUpdateVersion_Click);
+            // 
+            // txtNewVersion
+            // 
+            this.txtNewVersion.Location = new System.Drawing.Point(163, 92);
+            this.txtNewVersion.Name = "txtNewVersion";
+            this.txtNewVersion.ReadOnly = true;
+            this.txtNewVersion.Size = new System.Drawing.Size(149, 28);
+            this.txtNewVersion.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "New Version:";
+            // 
+            // btnPackage
+            // 
+            this.btnPackage.Location = new System.Drawing.Point(35, 250);
+            this.btnPackage.Name = "btnPackage";
+            this.btnPackage.Size = new System.Drawing.Size(316, 43);
+            this.btnPackage.TabIndex = 7;
+            this.btnPackage.Text = "3. Package current version";
+            this.btnPackage.UseVisualStyleBackColor = true;
+            this.btnPackage.Click += new System.EventHandler(this.btnPackage_Click);
+            // 
+            // btnBuild
+            // 
+            this.btnBuild.Location = new System.Drawing.Point(35, 201);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(316, 43);
+            this.btnBuild.TabIndex = 8;
+            this.btnBuild.Text = "2. Build release";
+            this.btnBuild.UseVisualStyleBackColor = true;
+            // 
+            // FormMian
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 334);
+            this.Controls.Add(this.btnBuild);
+            this.Controls.Add(this.btnPackage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNewVersion);
+            this.Controls.Add(this.btnUpdateVersion);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtVersion);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMian";
+            this.Text = "PasteEx.Deploy";
+            this.Load += new System.EventHandler(this.FormMian_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtVersion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnUpdateVersion;
+        private System.Windows.Forms.TextBox txtNewVersion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPackage;
+        private System.Windows.Forms.Button btnBuild;
+    }
+}
+
---
+++ PasteEx.Deploy/FormMian.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasteEx.Deploy
+{
+    public partial class FormMian : Form
+    {
+        readonly string path = new DirectoryInfo("../../../").FullName;
+        readonly string[] readmeFileNames = new string[] { "README.md","README_CN.md" }; 
+
+        public FormMian()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMian_Load(object sender, EventArgs e)
+        {
+            lblPath.Text = path;
+
+            GetSurrentVersion();
+        }
+
+        private void GetSurrentVersion()
+        {
+            // curr version
+            string assemblyInfoPath = Path.Combine(path, @"PasteEx\Properties\AssemblyInfo.cs");
+            string assemblyInfoContent = File.ReadAllText(assemblyInfoPath);
+
+            Match m = Regex.Match(assemblyInfoContent, "AssemblyFileVersion\\(\"(?<a>.*)\"\\)");
+            if (m.Success)
+            {
+                txtVersion.Text = m.Groups["a"].Value;
+            }
+        }
+
+        private void btnUpdateVersion_Click(object sender, EventArgs e)
+        {
+            // 1. AssemblyInfo.cs
+            string assemblyInfoPath = Path.Combine(path, @"PasteEx\Properties\AssemblyInfo.cs");
+            string assemblyInfoContent = File.ReadAllText(assemblyInfoPath);
+            assemblyInfoContent = assemblyInfoContent.Replace(txtVersion.Text, txtVersion.Text);
+            File.WriteAllText(assemblyInfoPath, assemblyInfoContent);
+            Console.WriteLine("AssemblyInfo.cs √");
+
+            // 2. README.md
+            foreach (string readmeFileName in readmeFileNames)
+            {
+                string readmeFilePath = Path.Combine(path, readmeFileName);
+                string content = File.ReadAllText(readmeFilePath);
+                File.WriteAllText(readmeFilePath, content.Replace(txtVersion.Text, txtVersion.Text));
+                Console.WriteLine(readmeFileName + " √");
+            }
+
+            GetSurrentVersion();
+
+            MessageBox.Show("Done!");
+
+        }
+
+        private void btnPackage_Click(object sender, EventArgs e)
+        {
+
+            // C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe
+            // .\MSBuild.exe -p:configuration="release" -t:rebuild "${path}"
+            // string packageScriptPatch = @"";
+        }
+    }
+}
